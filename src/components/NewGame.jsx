a6bc1699fc2652f722877872b17d10863f68a006
d3oxy <ishansachu1@gmail.com>
2022-12-03 08:22:31 +0000

    fix: bug

--- src/components/NewGame.jsx
+++ src/components/NewGame.jsx
@@ -7,15 +7,13 @@ const Header = ({ handleNewGame, wins }) => {
 
   return (
     <div className='newgame'>
-    {/* <header className="header"> */}
-      <a href="#" className='newgamebtn' onClick={handleNewGame}>
-          <span></span>
+      <p className='newgamebtn' onClick={handleNewGame}>
+      <span></span>
       <span></span>
       <span></span>
       <span></span>
       New Game
-    </a>
-    {/* </header> */}
+    </p>
     </div>
   );
 };
